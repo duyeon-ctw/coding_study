cbd8d7b58af4534f6d3db22837178ee57d5fb0f0
duyeon-ctw <duyoen.ctw@gmail.com>
2023-05-09 20:46:58 +0900

    JAVA

---
+++ JAVA/FourArithmetic.java
@@ -0,0 +1,19 @@
+public class FourArithmetic {
+    public static void main(String[] args) {
+        int a, b, sum;
+        a = 12;
+        b = 2;
+
+        sum = a + b;
+        System.out.println("a + b = " + sum);
+
+        sum = a - b;
+        System.out.println("a - b = " + sum);
+
+        sum = a / b;
+        System.out.println("a / b = " + sum);
+
+        sum = a * b;
+        System.out.println("a * b = " + sum);
+    }
+}/
