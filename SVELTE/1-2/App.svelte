58a5a6a900049f340636cb146ebad98a09f7aa0b
DUYEON <110620106+duyeon-ctw@users.noreply.github.com>
2024-04-30 20:04:08 +0900

    Create App.svelte

---
+++ SVELTE/1-2/App.svelte
@@ -0,0 +1,9 @@
+<script>
+  let count = 0; // state(상태값)
+
+  const handleClick = () => (count = count + 1); // count를 count + 1로 대할당
+</script>
+
+<button on:click={handleClick}>
+  클릭 수 {count} <!-- {} 기호로 마크업에 상태값 표시 -->
+</button>
