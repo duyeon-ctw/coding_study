4132abf1c70676eb444c0e507e01dbcc5423d232
DUYEON <110620106+duyeon-ctw@users.noreply.github.com>
2024-05-04 09:32:06 +0900

    Create App.svelte

---
+++ SVELTE/1-4/App.svelte
@@ -0,0 +1,31 @@
+<script>
+  let list = [
+    {
+      id: 1,
+      content: "첫 번째 할 일",
+      done: false,
+    },
+    {
+      id: 2,
+      content: "두 번째 할 일",
+      done: false,
+    },
+    {
+      id: 3,
+      content: "세 번째 할 일",
+      done: true,
+    },
+    {
+      id: 4,
+      content: "네 번째 할 일",
+      done: false,
+    },
+  ];
+</script>
+
+<ul>
+  {#each list as item}
+    <p>{item.id}</p>
+    <p>{item.content}</p>
+  {/each}
+</ul>
