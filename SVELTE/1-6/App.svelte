029ffb81912c8b6dc133968cb0cfff0af57df00c
DUYEON <110620106+duyeon-ctw@users.noreply.github.com>
2024-05-04 11:30:28 +0900

    Create App.svelte

---
+++ SVELTE/1-6/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let count = 1
+
+  function handleClick() {
+    count += 1
+  }
+  
+</script>
+
+<button on:click={handleClick}>
+  클릭수 {count}  {count == 1 ? 'time' : 'times'}
+</button>
