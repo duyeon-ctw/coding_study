4488d80a0b0d7d660afa2bc26f8d37807c7c7909
DUYEON <110620106+duyeon-ctw@users.noreply.github.com>
2024-05-10 19:19:43 +0900

    Create App.svelte

---
+++ SVELTE/1-9/App.svelte
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="handeClick">
+      Clicked {{count}} {{count == 1 ? 'time' : 'times'}}
+    </button>
+
+    <p>{{count}} doubled is {{doubled}}</p>
+  </div>
+  
+</template>
+
+<script>
+  export degault {
+    data () {
+      return {
+        count: 0
+      }
+    },
+    computed: {
+      doubled () {
+        return this.count * 2
+      }
+    },
+    watch: {
+      
+    }
+  }
+</script>
+
