a98f23d08388b303ab73d576944b11c9d89740ff
DUYEON <110620106+duyeon-ctw@users.noreply.github.com>
2024-05-10 19:59:22 +0900

    Create App.svelte

---
+++ SVELTE/1-11/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  function handelClick_1(param) {
+    alert('선택값 ${param}')
+  }
+
+  function handelClick_2(param) {
+    alert('선택값 ${param}')
+  }
+</script>
+
+<button on:click={() => handelClick_1('1번')}>1번 버튼</button>
+<button on:click={() => handelClick_2('2번')}>2번 버튼</button>
