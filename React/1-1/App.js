746421f8de3ecd6ddb04372be0ccf1aeb7b65c8d
DUYEON <110620106+duyeon-ctw@users.noreply.github.com>
2024-07-28 15:03:08 +0900

    Create App.js

---
+++ React/1-1/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+    <header>
+      <h1><a href='/'>WEB</a></h1>
+    </header>
+    <nav>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>
+        <li><a href="/read/3">js</a></li>
+      </ol>
+    </nav>
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+    </main>
+  )
+}
